3325d540e5aa456a93a080e25667a056f6db4efa
Marion FDE <marion.favre-d-echallens@student.ecp.fr>
2019-03-23 15:31:47 +0100

    Update gmda.py

--- gmda.py
+++ gmda.py
@@ -4,4 +4,14 @@ Created on Sat Mar 23 11:01:03 2019
 
 @author: Admin
 """
+import numpy as np
+import matplotlib.pyplot as plt
 
+### Task 2 : generating a mixture of Gaussian
+mean = [0, 100,9]
+cov = [[1, 0,0], [0, 100,0],[0,0,1]]
+m = np.random.multivariate_normal(mean, cov) # un point d'une gaussian mix 3D
+
+def gaussian_mix(d,N,t):
+    
+    
