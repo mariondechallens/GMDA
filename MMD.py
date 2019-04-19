dc56f41b59bcfa0a52af6246786f65a09c086316
linlaurent <laurent.lin@student.ecp.fr>
2019-04-19 15:54:55 +0800

    Add unbiased version

--- MMD.py
+++ MMD.py
@@ -10,7 +10,7 @@ class MMD_test(object):
     Inspired by "A Kernel Two-Sample Test", J. of Machine Learning Research (13), 2012, Gretton & Al.
     """
 
-    def __init__(self, kernel_class:Kernel, alpha=0.05, biased=True):
+    def __init__(self, kernel_class: Kernel, alpha=0.05, biased=True):
         self.kernel_class = kernel_class
         self.alpha = alpha
         self.biased = biased
@@ -22,6 +22,13 @@ class MMD_test(object):
         self.test_result = ""  # accepted or rejected
 
     def fit(self, X, Y, verbose=True):
+        """
+        Compute TST MMD test for a given set of samples X^(m) and Y^(n) using kernel self.kernel,
+        at given level self.alpha, using self.biased or not estimator.
+        :param X: first set of samples, of shape (n_features, m)
+        :param Y: second set of samples, of shape (n_features, n)
+        :param verbose: print results or not at the end
+        """
         assert X.shape == Y.shape
         self.X = X
         self.Y = Y
@@ -36,10 +43,15 @@ class MMD_test(object):
                 + np.sum(self.kernel[m:, m:]) / (n ** 2)
                 - 2 * np.sum(self.kernel[m:, :m]) / (m * n)
             )
-            self.compute_threshold()  # threshold depends on K which depends on kernel type
+
         else:
-            # TODO : implement unbiased estimator
-            raise NotImplementedError
+            self.T = (
+                (np.sum(self.kernel[:m, :m]) - np.sum(np.diag(self.kernel[:m, :m]))) / (m * (m - 1))
+                + (np.sum(self.kernel[:n, :n]) - np.sum(np.diag(self.kernel[:n, :n]))) / (n * (n - 1))
+                - 2 * np.sum(self.kernel[m:, :m]) / (m * n)
+            )
+
+        self.compute_threshold()  # threshold depends on K which depends on kernel type
 
         self.test_result = "accepted" if self.T <= self.threshold else "rejected"
 
@@ -57,13 +69,15 @@ class MMD_test(object):
         return text
 
     def compute_threshold(self):
+        """
+        :return: Acceptance threshold for H0 at threshold alpha given that the estimator is biased or not
+        """
         n = self.X.shape[1]
         K = self.kernel_class.K
         if self.biased:
             self.threshold = np.sqrt(2 * K / n) * (1 + np.sqrt(2 * np.log(1 / self.alpha)))  # need n=m
         else:
-            # TODO : implement unbiased estimator
-            raise NotImplementedError
+            self.threshold = 4 * K * np.sqrt(np.log(1 / self.alpha) / n)
 
     def get_results(self):
         return {"test_result": self.test_result, "test_statistic": self.T, "threshold": self.threshold}
